2920f6d033ed1a51a50d70499601255cb0bc541b
cd ~/code/$RobertPE96/dotfiles && zsh git_setup.sh <robert.p.eickmeyer@gmail.com>
2022-04-14 18:39:48 +0200

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
- 10.times do |restaurant|
-  Restaurant.create(
-    name: Faker::Restaurant.name,
-    address: Faker::Address.street_address,
-    phone_number: Faker::PhoneNumber.cell_phone_in_e164.to_s,
-    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
-  )
-end
